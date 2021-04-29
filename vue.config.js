0a96361b60869bbd5410abba5d8ff04493f60477
Eugene <mdwit0r@yandex.ru>
2021-04-29 15:48:34 +0300

    add svg-sprite-loader

--- vue.config.js
+++ vue.config.js
@@ -6,8 +6,19 @@ module.exports = {
       preProcessor: "scss",
       patterns: [
         path.resolve(__dirname, "./src/assets/scss/variables.scss"),
-        path.resolve(__dirname, "./src/assets/scss/functions.scss")
+        path.resolve(__dirname, "./src/assets/scss/functions.scss"),
       ],
     },
+    svgSprite: {
+      dir: "src/assets/icons",
+      test: /\.(svg)(\?.*)?$/,
+      loaderOptions: {
+        extract: false,
+      },
+    },
+  },
+
+  chainWebpack: (config) => {
+    config.module.rule("svg-sprite").use("svgo-loader").loader("svgo-loader");
   },
 };
