606a8c9953ed95728a570134fa4ac0d9ce61ccfd
Eugene <mdwit0r@yandex.ru>
2021-04-30 04:21:35 +0300

    add base markup in `CurrentWeather` component

---
+++ src/components/CurrentWeather.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="current-weather">
+    <div class="current-weather__result">
+      <StateWeather :name="stateName" />
+      <h2 class="current-weather__temperature">{{ temperature }}º</h2>
+    </div>
+    <p v-if="weatherAssessment" class="current-weather__assessment">
+      {{ weatherAssessment }}
+    </p>
+  </div>
+</template>
+
+<script>
+import StateWeather from "./icons/StateWeather";
+export default {
+  name: "CurrentWeather",
+  components: { StateWeather },
+  props: {
+    stateName: {
+      type: String,
+      required: true,
+    },
+    temperature: {
+      type: Number,
+      required: true,
+    },
+    weatherAssessment: {
+      type: String,
+      default: null,
+    },
+  },
+};
+</script>
+
+<style lang="scss"></style>
