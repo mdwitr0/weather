9d05957b84108c0ec1882ed24de3441a36106ebf
Eugene <mdwit0r@yandex.ru>
2021-04-30 04:21:56 +0300

    add base markup in `SearchCity` component

---
+++ src/components/SearchCity.vue
@@ -0,0 +1,30 @@
+<template>
+  <form @submit="setCity" class="search-city">
+    <input
+      id="search-city"
+      type="text"
+      name="city"
+      class="search-city__field"
+      placeholder="Название города"
+      aria-label="Название города"
+      aria-placeholder="Название города"
+    />
+    <button type="submit" class="search-city__button">ОК</button>
+  </form>
+</template>
+
+<script>
+export default {
+  name: "SearchCity",
+  data() {
+    return {
+      cityName: "",
+    };
+  },
+  methods: {
+    setCity() {},
+  },
+};
+</script>
+
+<style lang="scss"></style>
