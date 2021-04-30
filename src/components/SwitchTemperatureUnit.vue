645c5cc0101065dcd52633e45546737c2cd4e1d0
Eugene <mdwit0r@yandex.ru>
2021-04-30 04:22:17 +0300

    add base markup in `SwitchTemperatureUnit` component

---
+++ src/components/SwitchTemperatureUnit.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="switch-temperature-unit">
+    <span class="switch-temperature-unit__degree-symbol">º</span>
+    <div class="switch-temperature-unit__selection">
+      <label
+        for="celsius"
+        class="switch-temperature-unit__unit switch-temperature-unit__unit_type_celsius"
+        aria-label="По цельсию"
+        >C</label
+      >
+      <label
+        for="fahrenheit"
+        class="switch-temperature-unit__unit switch-temperature-unit__unit_type_fahrenheit"
+        aria-label="По фаренгейту"
+        >F</label
+      >
+      <input id="celsius" name="unit" type="radio" />
+      <input id="fahrenheit" name="unit" type="radio" />
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "SwitchTemperatureUnit",
+};
+</script>
+
+<style lang="scss"></style>
