34ddbf580345564ee354bdc29ff11c76d6cd4d43
Eugene <mdwit0r@yandex.ru>
2021-04-30 19:34:11 +0300

    add text from location button and add base styles `UserLocation`

--- src/components/UserLocation.vue
+++ src/components/UserLocation.vue
@@ -8,17 +8,20 @@
       <button class="user-location__search-city">Сменить город</button>
       <button class="user-location__search-location">
         <SvgIcon name="arrow" class="user-location__arrow-icon" />
+        Мое местоположение
       </button>
     </div>
+    <SearchCity />
   </div>
 </template>
 
 <script>
 import SvgIcon from "./icons/SvgIcon";
 import SwitchTemperatureUnit from "./SwitchTemperatureUnit";
+import SearchCity from "./SearchCity";
 export default {
   name: "UserLocation",
-  components: { SwitchTemperatureUnit, SvgIcon },
+  components: { SearchCity, SwitchTemperatureUnit, SvgIcon },
   props: {
     cityName: {
       type: String,
@@ -28,4 +31,35 @@ export default {
 };
 </script>
 
-<style lang="scss"></style>
+<style lang="scss">
+.user-location {
+  &__city {
+    font-size: font-size(3);
+    font-weight: weight(1);
+  }
+  &__arrow-icon {
+    $size: 20;
+    width: rem($size);
+    height: rem($size);
+    fill: color("light", 0.6);
+    transform: rotate(11deg);
+  }
+  &__search-city,
+  &__search-location {
+    font-size: font-size(0);
+
+    color: color("light", 0.6);
+    &:hover {
+      color: color();
+      & .user-location__arrow-icon {
+        fill: color("light");
+      }
+    }
+  }
+  &__search-location {
+    display: grid;
+    grid-template-columns: min-content 1fr;
+    grid-column-gap: rem(11);
+  }
+}
+</style>
