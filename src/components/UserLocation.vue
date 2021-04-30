f45a71d7918c58891651320e98a970cf068c9a1c
Eugene <mdwit0r@yandex.ru>
2021-04-30 04:22:39 +0300

    add base markup in `UserLocation` component

---
+++ src/components/UserLocation.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="user-location">
+    <h1 class="user-location__city">{{ cityName }}</h1>
+    <div class="user-location__buttons">
+      <button class="user-location__search-city">Сменить город</button>
+      <button class="user-location__search-location">
+        <SvgIcon name="arrow" class="user-location__arrow-icon" />
+      </button>
+    </div>
+  </div>
+</template>
+
+<script>
+import SvgIcon from "./icons/SvgIcon";
+export default {
+  name: "UserLocation",
+  components: { SvgIcon },
+  props: {
+    cityName: {
+      type: String,
+      required: true,
+    },
+  },
+};
+</script>
+
+<style lang="scss"></style>
