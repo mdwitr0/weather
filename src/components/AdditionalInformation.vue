b5cca26939a0cba5aefb0c5b7a441cbce591d3ea
Eugene <mdwit0r@yandex.ru>
2021-04-30 04:20:56 +0300

    add base markup in `AdditionalInformation` component

---
+++ src/components/AdditionalInformation.vue
@@ -0,0 +1,24 @@
+<template>
+  <div v-if="value" class="additional-information">
+    <span v-if="name" class="additional-information__name">{{ name }}</span>
+    <span class="additional-information__value">{{ value }}</span>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "AdditionalInformation",
+  props: {
+    name: {
+      type: String,
+      default: null,
+    },
+    value: {
+      type: String,
+      required: true,
+    },
+  },
+};
+</script>
+
+<style lang="scss"></style>
