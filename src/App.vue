5e5e4b7b8ced6d44198fcfa1d054b8400723c811
Eugene <mdwit0r@yandex.ru>
2021-05-01 20:53:13 +0300

    fixed pixel-perfect styles and add mobile adaptive styles from `app`

--- src/App.vue
+++ src/App.vue
@@ -7,24 +7,24 @@
     </header>
     <main class="forecast__main">
       <div class="forecast__container forecast__container_size_large">
-          <CurrentWeather
-              class="forecast__weather"
-              :state-name="stateName"
-              :weather-assessment="weatherAssessment"
-              :temperature="temperature"
-          />
+        <CurrentWeather
+          class="forecast__weather"
+          :state-name="stateName"
+          :weather-assessment="weatherAssessment"
+          :temperature="temperature"
+        />
       </div>
     </main>
     <footer class="forecast__footer">
       <div class="forecast__container">
         <div class="forecast__additional-list">
           <AdditionalInformation
-              class="forecast__additional"
-              v-for="(info, idx) in additionalInfo"
-              :key="idx"
-              :name="info.name"
-              :value="info.value"
-              :value-type="info.valueType"
+            class="forecast__additional"
+            v-for="(info, idx) in additionalInfo"
+            :key="idx"
+            :name="info.name"
+            :value="info.value"
+            :value-type="info.valueType"
           />
         </div>
       </div>
@@ -78,10 +78,12 @@ export default {
   grid-template-rows: min-content 1fr min-content;
   box-sizing: border-box;
   padding: rem(75) 0 rem(122) 0;
+  @media (max-width: 640px) {
+    padding: rem(19) 0 rem(41) 0;
+  }
   height: 100vh;
   width: 100vw;
   background: color("nice");
-
   &__container {
     margin: 0 auto;
     max-width: rem(1215);
@@ -93,12 +95,33 @@ export default {
   }
   &__additional-list {
     display: grid;
-    grid-template-columns: repeat(auto-fit, minmax(200px, 1fr));
+    grid-template-columns: repeat(auto-fit, minmax(rem(150), 1fr));
     grid-column-gap: rem(55);
+    grid-row-gap: rem(30);
     margin: 0 rem(25);
+    @media (max-width: 932px) {
+      grid-template-columns: repeat(2, minmax(rem(50), 1fr));
+    }
+    @media (max-width: 640px) {
+      grid-column-gap: rem(11);
+      grid-row-gap: rem(34);
+    }
   }
   &__weather {
-    margin-bottom: rem(54);
+    margin-bottom: rem(47);
+    @media (max-width: 640px) {
+      margin-bottom: rem(30);
+    }
   }
+  &__user-location {
+    margin-left: rem(24);
+    @media (max-width: 1239px) {
+      margin: 0 rem(24);
+    }
+    @media (max-width: 640px) {
+      margin: 0 rem(30) 0 rem(18);
+    }
+  }
+
 }
 </style>
