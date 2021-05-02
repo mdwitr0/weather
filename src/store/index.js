f4bf33208d061c3f6e034387affb3782d1744366
Eugene <mdwit0r@yandex.ru>
2021-05-03 02:48:19 +0300

    add persistedstate and secure-ls

--- src/store/index.js
+++ src/store/index.js
@@ -1,11 +1,20 @@
 import Vue from "vue";
 import Vuex from "vuex";
+import createPersistedState from "vuex-persistedstate";
+import SecureLS from "secure-ls";
+const ls = new SecureLS({ isCompression: false });
 
 Vue.use(Vuex);
 
 export default new Vuex.Store({
-  state: {},
-  mutations: {},
-  actions: {},
+  plugins: [
+    createPersistedState({
+      storage: {
+        getItem: (key) => ls.get(key),
+        setItem: (key, value) => ls.set(key, value),
+        removeItem: (key) => ls.remove(key),
+      },
+    }),
+  ],
   modules: {},
 });
