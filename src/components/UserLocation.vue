2a87a9ac31d55a939c5b6e85ae7c97bdcf394573
Eugene <mdwit0r@yandex.ru>
2021-05-04 05:08:25 +0300

    animate search change

--- src/components/UserLocation.vue
+++ src/components/UserLocation.vue
@@ -1,10 +1,10 @@
 <template>
   <div class="user-location">
-    <div class="user-location__info">
+    <div class="user-location__info" :class="{ 'user-location__info_visible_false': visibilitySearchForm }">
       <h1 class="user-location__city">{{ cityName }}</h1>
       <SwitchTemperatureUnit class="user-location__switch-temp" />
     </div>
-    <div class="user-location__buttons">
+    <div class="user-location__buttons" :class="{ 'user-location__buttons_visible_false': visibilitySearchForm }">
       <button
         @click="switchSearchForm(!visibilitySearchForm)"
         class="user-location__search-city"
@@ -75,6 +75,7 @@ export default {
   &__city {
     font-size: font-size(3);
     font-weight: weight(1);
+    @include base-animate(opacity);
     @media (max-width: 640px) {
       font-size: font-size(2, true);
     }
@@ -90,6 +91,16 @@ export default {
     display: grid;
     grid-template-columns: 1fr min-content;
     align-items: flex-start;
+    &_visible_false {
+      .user-location__city {
+        opacity: 0;
+      }
+      @media (max-width: 640px ){
+        .user-location__switch-temp {
+          opacity: 0;
+        }
+      }      
+    }
   }
   &__search-city,
   &__search-location {
@@ -116,6 +127,7 @@ export default {
   &__search {
     position: absolute;
     top: 0;
+    @include base-animate(opacity);
     opacity: 0;
     visibility: hidden;
     &_visible_true {
@@ -131,9 +143,14 @@ export default {
     display: grid;
     grid-template-columns: repeat(2, max-content);
     grid-column-gap: rem(25);
+    @include base-animate(opacity);
+    &_visible_false {
+      opacity: 0;
+    }
   }
   &__switch-temp {
     margin-top: rem(6);
+    @include base-animate(opacity);
     @media (max-width: rem(640)) {
       margin-right: rem(14);
     }
